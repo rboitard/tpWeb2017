e7c11c69f9f49a3d9dde3935f7c9b38d3e31592f
romain <rboitard@gmail.com>
2018-03-14 20:59:48 +0100

    add paint methode

--- view.js
+++ view.js
@@ -1,2 +1,47 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Line.prototype.paint = function (ctx) {
+    //TODO Manager color
+
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.epaisseur;
+    ctx.moveTo(this.coordXBegin, this.coordYBegin);
+    ctx.lineTo(this.coordXEnd, this.coordYEnd);
+    ctx.stroke();
+
+};
+
+Rectangle.prototype.paint = function (ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.epaisseur;
+    ctx.rect(this.coordX, this.coordY, this.largeur, this.hauteur);
+    ctx.stroke();
+};
+
+
+Drawing.prototype.paint = function (ctx) {
+    console.log(this.getForme());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForme().forEach(function (forme) {
+        forme.paint(ctx);
+    });
+};
+
+
+
+function updateShapeList(drawing){
+
+    var shapeList = document.getElementById('shapeList');
+
+    var butt = document.createElement("button");
+    butt.setAttribute("class", "btn btn-default");
+    butt.setAttribute("type", "button");
+
+    var span = document.createElement("span");
+    span.setAttribute("class", "glyphicon glyphicon - remove - sign");
+    butt.appendChild(span);
+
